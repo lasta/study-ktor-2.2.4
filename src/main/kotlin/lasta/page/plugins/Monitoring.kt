dee2ec2723a11bc63289f6befc8053e1e3a7782f
Atsushi Koshikizawa <lastarrow21@gmail.com>
2023-03-25 16:06:11 +0900

    Delete SLF4J reporter

--- src/main/kotlin/lasta/page/plugins/Monitoring.kt
+++ src/main/kotlin/lasta/page/plugins/Monitoring.kt
@@ -1,17 +1,20 @@
 package lasta.page.plugins
 
-import io.ktor.server.plugins.callloging.*
-import org.slf4j.event.*
-import io.ktor.server.request.*
-import io.ktor.http.*
-import io.ktor.server.plugins.callid.*
-import io.ktor.server.metrics.dropwizard.*
-import com.codahale.metrics.*
-import io.micrometer.prometheus.*
-import io.ktor.server.metrics.micrometer.*
-import io.ktor.server.response.*
-import io.ktor.server.application.*
-import io.ktor.server.routing.*
+import io.ktor.http.HttpHeaders
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.application.install
+import io.ktor.server.metrics.micrometer.MicrometerMetrics
+import io.ktor.server.plugins.callid.CallId
+import io.ktor.server.plugins.callid.callIdMdc
+import io.ktor.server.plugins.callloging.CallLogging
+import io.ktor.server.request.path
+import io.ktor.server.response.respond
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
+import org.slf4j.event.Level
 
 fun Application.configureMonitoring() {
     install(CallLogging) {
@@ -24,14 +27,6 @@ fun Application.configureMonitoring() {
         verify { callId: String ->
             callId.isNotEmpty()
         }
-    }
-    install(DropwizardMetrics) {
-        Slf4jReporter.forRegistry(registry)
-            .outputTo(this@module.log)
-            .convertRatesTo(TimeUnit.SECONDS)
-            .convertDurationsTo(TimeUnit.MILLISECONDS)
-            .build()
-            .start(10, TimeUnit.SECONDS)
     }
     val appMicrometerRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
 
