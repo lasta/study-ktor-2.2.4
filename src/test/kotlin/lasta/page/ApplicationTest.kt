872a93900f1728e540eeeaa49d41db241c726b90
Atsushi Koshikizawa <lastarrow21@gmail.com>
2023-03-25 19:04:52 +0900

    fix tests to load routing

--- src/test/kotlin/lasta/page/ApplicationTest.kt
+++ src/test/kotlin/lasta/page/ApplicationTest.kt
@@ -1,17 +1,21 @@
 package lasta.page
 
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.server.testing.*
-import kotlin.test.*
-import io.ktor.http.*
-import lasta.page.plugins.*
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.routing.routing
+import io.ktor.server.testing.testApplication
+import lasta.page.controller.routeSamples
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
         application {
-            configureRouting()
+            routing {
+                routeSamples()
+            }
         }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
